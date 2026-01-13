510234554032db4d6d4e30523fb15334df69a7f2
Printkaue <kaue0987virgulino@gmail.com>
2026-01-13 14:07:07 -0300

    add fetch

--- BR-K-Fut/backend/main.py
+++ BR-K-Fut/backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import os
 from werkzeug.utils import secure_filename
 from supabase import create_client
@@ -6,7 +7,7 @@ from dotenv import load_dotenv
 
 #configuraçoes
 app = Flask(__name__)
-
+CORS(app)
 load_dotenv()
 
 supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_SERVICE_ROLE_KEY"))
@@ -16,6 +17,13 @@ supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_SERVICE_
 @app.route("/times", methods=["GET"])
 def gettimes():
     pass
+
+@app.route("/rodada", methods=["GET"])
+def getgames():
+    rodata = request.args.get("n", type=int)
+    res = supabase.table("Resultados").select("*").eq("rodata", rodata).execute()
+
+    return jsonify(res.data)
 
 if __name__ == "__main__":
     app.run()
--- BR-K-Fut/src/App.tsx
+++ BR-K-Fut/src/App.tsx
@@ -2,11 +2,44 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [rodada, setRodada] = useState("");
+  const [jogos, setJogos] = useState([]);
+  const [loading, setloading] = useState(false);
+
+  const buscar = () => {
+    if (!rodada){
+      return;
+    }
+
+    setloading(true);
+
+     fetch(`http://localhost:5000/rodada?n=${rodada}`)
+      .then(res => res.json())
+      .then(data => {
+        setJogos(data);
+        setloading(false);
+      });
+  };
 
   return (
     <>
-      
+      <h1>Selecione rodada</h1>
+      <input type="number" placeholder="digite um numero" onChange={e => setRodada(e.target.value)}></input>
+
+      <button onClick={buscar}>Buscar rodada</button>
+
+      {loading && <p>carregando...</p>}
+
+      <ul>
+        {jogos.map(jogo => (
+          <li key={jogo.ID}>
+            {jogo.data} {" "} {"-"} {" "}
+            {jogo.mandante} {jogo.mandante_Placar} x{" "}
+            {jogo.visitante_Placar} {jogo.visitante}
+          </li>
+        ))}
+      </ul>
+
     </>
   )
 }
